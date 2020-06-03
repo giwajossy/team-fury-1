cf0558b67121692d111b78afa4e82b9a64b742ad
giwajossy <giwajosef@gmail.com>
2020-06-03 05:20:06 +0100

    Created scripts/jossy.js, and coded the ExpectedOutput

---
+++ scripts/jossy.js
@@ -0,0 +1,14 @@
+
+var firstName = "Jossy"; // First Name
+var lastName = "Giwa"; // Last Name
+
+var HNGi7_ID = "HNG-01584"; // HNGi7 Internship ID
+var language = "Javascript"; // Language
+
+expectedOutput = "Hello World, this is " + lastName + " " + firstName + " with HNGi7 ID " + HNGi7_ID + " using " + language + " for stage 2 task";
+alert (expectedOutput);
+
+/*
+Model for expected result
+Hello World, this is [full name] with HNGi7 ID [ID] using [language] for stage 2 task
+*/
